9f473022a8f0dd710015d224ca94f6ad724494b0
Савенчук Любовь <sav_lu@gmail.com>
2022-09-05 13:31:33 +0500

    ex2/

---
+++ ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе, которые являются делителями этого числа
+
+Console.WriteLine("Input any number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int c;
+//извлечение цифр числа
+while(x!=0)
+{
+   c = x % 10;
+   Console.WriteLine(c + " ");
+   x = x / 10;
+}
+// проверка на делитель и нахождение их суммы
+
+if(x % c == 0)
+{
+   sum = sum + c;
+   Console.WriteLine(sum);
+}
+
